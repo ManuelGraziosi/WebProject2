5745c572a16aa77ceabbeb9fff2c96b21687ad8a
daniele <daniele@daniele-HP>
2015-08-31 12:00:39 +0200

    js completamente strutturato ad oggetti ed eventi

--- JS/Controller/C_home_eventi.js
+++ JS/Controller/C_home_eventi.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var C,V;
+var C_home;
 
 var C_home_eventi = function(){
     C_home = new C_home();
