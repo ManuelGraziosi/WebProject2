726949c909bc1d7e83a3dbe8fced11df356d18f9
daniele <daniele@daniele-HP>
2015-12-02 11:27:49 +0100

    inserimento C_fareOrdine e V_fareOrdine

--- PHP/Utility/U_operazioni.class.php
+++ PHP/Utility/U_operazioni.class.php
@@ -7,7 +7,7 @@
 class U_operazioni {
     
     public static function inserisciDati(&$oggetto, $dati){
-        if(!array_key_exists('NOME_PRODOTTO', $dati)){
+        if(array_key_exists('0', $dati)){
             $dati=$dati[0];
         }
         foreach ($dati as $key => $value) {
