fe1d1fe541e8ae61c5f08832e5e12fcdaffdc172
ManuelGraziosi <manuel.graziosi@gmail.com>
2016-02-05 12:14:58 +0100

    Lavorato su Dialog "Dettagli Prodotto". Ripulito Index.PHP

--- index.php
+++ index.php
@@ -4,11 +4,5 @@ require_once 'PHP/includes/config.php';
 /**/
 $index=new C_home();
 $index->esegui();
-/**
-$db= new F_database();
-$parametri= array(array("nome_prod_com","=","Caciotta","AND"),array("testo","=","pippo",""));
-print_r($parametri);print("<br>");
-$d= $db->ricerca("commento", $parametri);
-print_r($d);
-/**/
+
 ?>
