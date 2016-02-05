0bff41cb64866cbf5653b838e0a5f54be6f1da1b
ManuelGraziosi <manuel.graziosi@gmail.com>
2016-02-05 12:13:36 +0100

    Lavorato su Dialog "Dettagli Prodotto". Perfezionamento del Controllore PHP del commento

--- PHP/Controller/C_commento.class.php
+++ PHP/Controller/C_commento.class.php
@@ -14,12 +14,28 @@
 class C_commento {
     //put your code here
      public function inserisci_commento() {
+         //qui pescoi dati della sessione, nello specifico prendo la email
+        $sessione=  U_singolaistanza::getIstanza('U_sessione');
+        //$sessione=new U_sessione();
+        $EmailCom=$sessione->leggi_valore("email");
+        
+        //qui creo 
+        $view=U_singolaistanza::getIstanza('V_commento');
+        //$view=new V_commento();//serve solo per richiamare velocemente i metodi
+         //prelevamento dei dati da mettere in db dalla view
+        $ProductCOM = $view->getProductCom();
+        $DateCOM = $view->getDateCOM();
+        $TextCOM = $view->getTextCOM();
+        
+        //creazione dell'oggetto commento con relativi valori, per inserimento in DB
+        $commento = new E_commento();
+        $commento->getData_commento($DateCOM);
+        $commento->getNome_prod_com($ProductCOM);
+        $commento->getTesto($TextCOM);
+        $commento->getUser_commento($EmailCom);
+        $commento->getVoto(NULL);
+        
          
-         $view=U_singolaistanza::getIstanza('V_commento');
-         $view=new V_commento();//serve solo per richiamare velocemente i metodi
-         $commento=new V_commento();
-         
-         $email->setEMAIL($view->getEmail());
          $db=new F_database();
          $db->depositaRiga("newsletter", $email);
          
